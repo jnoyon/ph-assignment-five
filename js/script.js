38759b6c60de95efec3e6b49e5e5289896d352ce
jnoyon <jihadur51@gmail.com>
2024-09-24 15:53:49 +0600

    dom created

--- js/script.js
+++ js/script.js
@@ -7,13 +7,15 @@ const historySection = document.getElementById('history-section');
 const donationSection = document.getElementById('donation-section');
 
 
+const donationList = document.getElementById('donation-list');
+
 // donation card one
 const firstDonationBalance = document.getElementById('first-donation-balance');
 const firstDonationInput = document.getElementById('first-donation-input');
 const firstDonationButton = document.getElementById('first-donation-button');
 
 
-
+// Tab Section
 donationTab.addEventListener('click', function(){
     historySection.classList.add('hidden');
     donationSection.classList.remove('hidden');
@@ -21,7 +23,6 @@ donationTab.addEventListener('click', function(){
     historyTab.classList.remove('bg-lime-400', 'rounded-md', 'border-solid', 'border-lime-400', 'border');
     historyTab.classList.add('border-gray-300', 'border-solid', 'border', 'rounded-md');
 });
-
 
 historyTab.addEventListener('click', function(){
     historySection.classList.remove('hidden');
@@ -38,13 +39,22 @@ firstDonationButton.addEventListener('click', function(e){
     
 
     const firstDonationButtonN = parseFloat(firstDonationInput.value);
-        const firstDonationBalanceU = parseFloat(firstDonationBalance.innerText) + firstDonationButtonN;
-        firstDonationBalance.innerText = firstDonationBalanceU;
-        firstDonationInput.value = ''
+    const firstDonationBalanceU = parseFloat(firstDonationBalance.innerText) + firstDonationButtonN;
+    firstDonationBalance.innerText = firstDonationBalanceU;
+    firstDonationInput.value = ''
 
-        const currentBalanceU = parseFloat(currentBalance.innerText) - firstDonationButtonN;
-        currentBalance.innerText = currentBalanceU;
-        modal.showModal();
+    const currentBalanceU = parseFloat(currentBalance.innerText) - firstDonationButtonN;
+    currentBalance.innerText = currentBalanceU;
+    modal.showModal();
+
+    const div = document.createElement('div');
+    div.className = 'item gap-5 border border-gray-300 border-solid p-5 rounded-md mb-5'
+    div.innerHTML = `
+        <h1 class="font-bold text-xl mb-3"> ${firstDonationButtonN} Taka is Donated for the </h1>
+        <p class="text-gray-600"> ${new Date().toLocaleDateString()} </p>
+    `
+    donationList.appendChild(div);
+
 });
 
 
